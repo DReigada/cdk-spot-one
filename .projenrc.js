ca4a8a6c1904c93dda3e1740ae3a7044547fee8c
Pahud Hsieh <pahudnet@gmail.com>
2021-09-06 10:06:26 +0800

    bump versions (#394)

--- .projenrc.js
+++ .projenrc.js
@@ -1,14 +1,13 @@
 const { AwsCdkConstructLibrary, DependenciesUpgradeMechanism } = require('projen');
 
+const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
+
 const PROJECT_NAME = 'cdk-spot-one';
 const PROJECT_DESCRIPTION = 'One spot instance with EIP and defined duration. No interruption.';
-const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
 
 const project = new AwsCdkConstructLibrary({
   authorName: 'Pahud Hsieh',
   authorEmail: 'pahudnet@gmail.com',
-  authorUrl: 'https://github.com/pahud',
-  authorOrganization: true,
   name: PROJECT_NAME,
   description: PROJECT_DESCRIPTION,
   repository: 'https://github.com/pahud/cdk-spot-one.git',
@@ -18,6 +17,16 @@ const project = new AwsCdkConstructLibrary({
     announce: false,
   },
   defaultReleaseBranch: 'main',
+  cdkVersion: '1.77.0',
+  cdkDependencies: [
+    '@aws-cdk/aws-iam',
+    '@aws-cdk/aws-ec2',
+    '@aws-cdk/aws-lambda',
+    '@aws-cdk/aws-logs',
+    '@aws-cdk/core',
+    '@aws-cdk/custom-resources',
+  ],
+  minNodeVersion: '12.20.0',
   depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
     ignoreProjen: false,
     workflowOptions: {
@@ -29,24 +38,12 @@ const project = new AwsCdkConstructLibrary({
     secret: 'GITHUB_TOKEN',
     allowedUsernames: ['pahud'],
   },
-  cdkVersion: '1.77.0',
-  cdkDependencies: [
-    '@aws-cdk/aws-iam',
-    '@aws-cdk/aws-ec2',
-    '@aws-cdk/aws-lambda',
-    '@aws-cdk/aws-logs',
-    '@aws-cdk/core',
-    '@aws-cdk/custom-resources',
-  ],
   python: {
     distName: 'cdk-spot-one',
     module: 'cdk_spot_one',
   },
 });
 
-project.package.addField('resolutions', {
-  xmldom: 'github:xmldom/xmldom#0.7.0',
-});
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log'];
 project.npmignore.exclude(...common_exclude);
